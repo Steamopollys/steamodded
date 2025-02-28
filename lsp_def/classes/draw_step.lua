890c1acd9ef0058d907fadeb6e008cc6a1a49dd1
ItsFlowwey <73145114+GauntletGames-2086@users.noreply.github.com>
2025-02-28 12:59:12 -0900

    Update `SMODS.DrawStep` PR annotations

--- lsp_def/classes/draw_step.lua
+++ lsp_def/classes/draw_step.lua
@@ -3,6 +3,7 @@
 ---@class SMODS.DrawStep: SMODS.GameObject
 ---@field order? number Sets the order. `DrawStep` objects are evaluated in order from lowest to highest. 
 ---@field layers? table<string, true> Strings corresponding to draw layers. The `DrawStep` object's `func` will only be called when the `layer` arg in `Card:draw()` matches a string in this table. 
+---@field conditions? table<string, any> Table of conditions. This object will not draw if any condition is not `true` when evaluated. 
 ---@field __call? fun(self: SMODS.DrawStep|table, o: SMODS.DrawStep|table): nil|table|SMODS.DrawStep
 ---@field extend? fun(self: SMODS.DrawStep|table, o: SMODS.DrawStep|table): table Primary method of creating a class. 
 ---@field check_duplicate_register? fun(self: SMODS.DrawStep|table): boolean? Ensures objects already registered will not register. 
@@ -18,6 +19,8 @@
 ---@field take_ownership? fun(self: SMODS.DrawStep|table, key: string, obj: SMODS.DrawStep|table, silent?: boolean): nil|table|SMODS.DrawStep Takes control of vanilla objects. Child class must have get_obj for this to function
 ---@field get_obj? fun(self: SMODS.DrawStep|table, key: string): SMODS.DrawStep|table? Returns an object if one matches the `key`. 
 ---@field func? fun(card: Card|table, layer?: string) Handles the drawing logic of the `DrawStep`. 
+---@field check_conditions? fun(self: SMODS.DrawStep|table, card: Card|table, layer: string): boolean? Iterates over the `conditions` table, calling `check_individual_condition` per element`. 
+---@field check_individual_condition? fun(self: SMODS.DrawStep|table, card: Card|table, layer: string, k: string, v: any): boolean? Checks individual `SMODS.DrawStep` conditions, with `k` and `v` as key and value of condition. 
 ---@overload fun(self: SMODS.DrawStep): SMODS.DrawStep
 SMODS.DrawStep = setmetatable({}, {
     __call = function(self)
@@ -27,3 +30,9 @@ SMODS.DrawStep = setmetatable({}, {
 
 ---@type table<string, SMODS.DrawStep|table>
 SMODS.DrawSteps = {}
+
+---@type table<string, true>
+-- All keys in this table will not be automatically drawn with a default `draw()` call in the "others" DrawStep.
+SMODS.draw_ignore_keys = {
+    focused_ui = true, front = true, back = true, soul_parts = true, center = true, floating_sprite = true, shadow = true, use_button = true, buy_button = true, buy_and_use_button = true, debuff = true, price = true, particles = true, h_popup = true
+}
